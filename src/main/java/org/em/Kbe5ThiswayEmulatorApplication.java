4d78f77b1edad2568bbd5915ff01b615d544395a
devyoon <wideskyinme@gmail.com>
2025-05-22 16:53:28 +0900

    init

---
+++ src/main/java/org/em/Kbe5ThiswayEmulatorApplication.java
@@ -0,0 +1,13 @@
+package org.em;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kbe5ThiswayEmulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kbe5ThiswayEmulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/em/Kbe5ThiswayEmulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.em;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kbe5ThiswayEmulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
